47f97193d15c8756c5b53cded21bc889decb6bdb
qi-katherene <qikat93@gmail.com>
2026-01-15 12:01:17 -0500

    carousel snapback

--- script.js
+++ script.js
@@ -19,3 +19,28 @@ function scrollToProject(projectTitle) {
   });
 }
 
+const track = document.querySelector('.carousel-track');
+const cards = document.querySelectorAll('.carousel-card');
+
+function nextSlide() {
+  const cardWidth = cards[0].offsetWidth + 20;
+  track.scrollLeft += cardWidth;
+
+  // wrap to start
+  if (track.scrollLeft + track.clientWidth >= track.scrollWidth - 5) {
+    track.scrollTo({ left: 0, behavior: 'smooth' });
+  }
+}
+
+function prevSlide() {
+  const cardWidth = cards[0].offsetWidth + 20;
+  track.scrollLeft -= cardWidth;
+
+  // wrap to end
+  if (track.scrollLeft <= 0) {
+    track.scrollTo({
+      left: track.scrollWidth,
+      behavior: 'smooth'
+    });
+  }
+}
